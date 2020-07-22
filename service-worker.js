2ccb2a94401b08ee0cca820f271d4482167e5d43
soapYE <1614744147@qq.com>
2020-07-22 13:48:08 +0800

    second try

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-importScripts("/precache-manifest.5757e4740ca594eff2ca9e3475a285db.js", "/workbox-v4.3.1/workbox-sw.js");
-workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+importScripts("precache-manifest.747da7481f892f7f12131318bcb35c56.js", "workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
