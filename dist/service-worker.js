3ae6a627d7dfd52bbf1563c0085be823dd8777b7
soapYE <1614744147@qq.com>
2020-07-22 11:59:09 +0800

    hello

---
+++ dist/service-worker.js
@@ -0,0 +1,16 @@
+importScripts("/precache-manifest.5757e4740ca594eff2ca9e3475a285db.js", "/workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "/workbox-v4.3.1"});
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate(0),
+)
+
+workbox.routing.registerRoute(
+  new RegExp('https://reqres.in'),
+  workbox.strategies.networkFirst(),
+)
+
